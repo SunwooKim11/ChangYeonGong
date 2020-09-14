8fd8b2f22684806d31d9526973fb6e865baceae1
SunwooKim11 <seonu2001@naver.com>
2020-09-14 16:47:15 +0900

    Update 05_practice_2

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -1,27 +1,24 @@
 #define PIN_LED 7
-unsigned int cnt, toggle;
+unsigned int cnt,toggle;
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
   cnt=toggle=0;
-  digitalWrite(PIN_LED, toggle);
 }
-
-void loop() {
+void loop(){
+  digitalWrite(PIN_LED, toggle);
   delay(1000);
   while(cnt<10){
-  toggle =toggle_state(toggle);
-  digitalWrite(PIN_LED, toggle);
-  delay(100);
-  ++cnt;
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle);
+    delay(100);
+    ++cnt;
   }
-  toggle =toggle_state(toggle);
-  digitalWrite(PIN_LED, toggle);
+  digitalWrite(PIN_LED, 1);
   while(1){
-    ;
+    delay(1000);
   }
 }
-
 int toggle_state(int toggle){
   return !(toggle);
 }
